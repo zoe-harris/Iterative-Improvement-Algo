abe60c584bccca415bc53bcc08b90005cfa2bc80
Your Name <you@example.com>
2019-09-28 12:45:33 -0800

    Altered simulated_annealing to include schedule lambda function.

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -7,10 +7,13 @@
     def simulated_annealing(self):
 
         (given schedule is a mapping from time to T)
+        temperature_decrease = 0.1
+        schedule = lambda t : 1 - (t * temperature_decrease)
 
-        while True:
+        for t = 0 to infinity:
 
-            T = schedule[T]
+            T = schedule(t)
+
             if T = 0:
                 return current
 
@@ -18,6 +21,7 @@
             delta_E = next.change - current.change
 
             # If e^delta_E / T is less than 1, we choose the next investment & deal with T
+
             if (delta_E > 0) or (exp(delta_E / T) < 1):
                 next.investment += (current.investment * 0.1)
                 current.investment -= (current.investment * 0.1)
