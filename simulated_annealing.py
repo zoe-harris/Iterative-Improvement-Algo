544f039ade7b11557b5821a1d04096fac97389d6
Your Name <you@example.com>
2019-10-01 09:37:48 -0800

    Wrote initial version of Simulated Annealing

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,14 +1,15 @@
 # Zoe Harris & Rachel Lewis
 # Programming Assignment 2
 # CSCE405 Artificial Intelligence
-import math
+from math import *
+import random
+
 
 class SimulatedAnnealing:
 
     def __init__(self, portfolio):
         self.portfolio = portfolio
 
-"""
     def simulated_annealing(self):
 
         # schedule maps t to temperature
@@ -18,44 +19,24 @@ class SimulatedAnnealing:
         # start with first company in portfolio
         current = self.portfolio.investments[0]
 
-        for t in range(int(math.inf)):
+        # Run simulated annealing until temperature reaches 0
+        for t in range(int(inf)):
 
+            # Use schedule function to get value of T
             T = schedule(t)
 
+            # When T reaches zero, break from loop
             if T == 0:
                 return current
 
             # next = random node from portfolio
-            delta_E = next.change - current.change
+            next_index = random.randrange(0, 10)
+            next = self.portfolio.investments[next_index]
 
-            # If e^delta_E / T is less than 1, we choose the next investment & deal with T
+            # Calculate the value of Delta E
+            delta_E = next.percent_change - current.percent_change
 
+            # If (Delta E > 0) or if [(e^(Delta E) / T) < 1], choose the next investment
             if (delta_E > 0) or (exp(delta_E / T) < 1):
                 next.investment += (current.investment * 0.1)
                 current.investment -= (current.investment * 0.1)
-"""
-"""
-
-    def simulated_annealing(self):
-
-        (given schedule is a mapping from time to T)
-        temperature_decrease = 0.1
-        schedule = lambda t : 1 - (t * temperature_decrease)
-
-        for t = 0 to infinity:
-
-            T = schedule(t)
-
-            if T = 0:
-                return current
-
-            next = rand node from portfolio
-            delta_E = next.change - current.change
-
-            # If e^delta_E / T is less than 1, we choose the next investment & deal with T
-
-            if (delta_E > 0) or (exp(delta_E / T) < 1):
-                next.investment += (current.investment * 0.1)
-                current.investment -= (current.investment * 0.1)
-
-"""
