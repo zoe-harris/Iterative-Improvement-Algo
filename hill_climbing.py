03ccc012b421f9588fc8f2d284f201f6e5ed00f7
raelewis <relewis@me.com>
2019-10-06 21:06:12 -0800

    Basic Build up of hill climbing has been done. Two things need to be implemented before it is ready for testing. 1. Node / Company selection. 2. Random Restart.

--- hill_climbing.py
+++ hill_climbing.py
@@ -1,6 +1,73 @@
 # Zoe Harris & Rachel Lewis
 # Programming Assignment 2
 # CSCE405 Artificial Intelligence
+import copy
+
+ITERATION_COUNT = 4
+CHECK_COUNT = 3
+
+
+class HillClimbing:
+    def __init__(self, portfolio):
+        self.portfolio = portfolio
+        self.maximum_fitness = 1
+        self.same_max_reached = 0
+        self.max_unchanged = 0
+        self.curr_max_fitness = 1
+
+    def reinvest(self, next, current):
+        next.value += (current.value * 0.1)
+        current.value -= (current.value * 0.1)
+
+    def random_restart(self):
+        print("Congratz. Lets mix things up a little.")
+
+    def hill_climbing(self):
+        maximum = 0  # Best fitness value found
+        curr_maximum = 0
+        same_max_reached = 0  # This determines if we've peaked at the same point
+        max_unchanged = 0  # This determines if we have yet to beat the current maximum
+
+        curr_node, neighbor_node, x, counter = 0
+
+        # We will run Hill Climbing w/ Random Restart 4 times
+        while counter < ITERATION_COUNT:
+
+            # TO DO: Determine random restart functionality
+            if counter > 0:
+                self.random_restart()
+
+            # This loop runs the current iteration of hill climbing
+            while (max_unchanged <= CHECK_COUNT) or (same_max_reached <= CHECK_COUNT):
+
+                # TO DO: Determine how nodes are chosen to be compared in the loop
+
+                # If company 1 made less than company 2, reinvest
+                if curr_node.percent_change < neighbor_node.percent_change:
+                    self.reinvest(neighbor_node, curr_node)
+
+                new_fitness = self.portfolio.fitness()
+
+                # If our current maximum value < new fitness, change the fitness & reset values to 0
+                if curr_maximum < new_fitness:
+                    curr_maximum = self.portfolio.fitness()
+                    same_max_reached = 0
+                    max_unchanged = 0
+
+                # If we have yet to find a new peak in this run, increment
+                if curr_maximum > new_fitness:
+                    max_unchanged += 1
+
+                # If we have found the same maximum, increment
+                if curr_maximum == new_fitness:
+                    same_max_reached += 1
+
+            # If our current maximum is greater than the best maximum we've found, save it
+            if curr_maximum > maximum:
+                maximum = curr_maximum
+
+            curr_maximum, max_unchanged, same_max_reached = 0
+            counter += 1
 
 """
 
