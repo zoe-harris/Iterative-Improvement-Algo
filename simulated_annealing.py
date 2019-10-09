f2c52d976558fab748c4786c18327e2b0cf1f642
Your Name <you@example.com>
2019-10-09 09:20:06 -0800

    Adjusted simulated annealing to calculate delta E by subtracting the fitness of the current portfolio from the fitness of a portfolio in which 10% of the 'current' stock has been invested into the 'next' stock

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -2,9 +2,9 @@
 # Programming Assignment 2
 # CSCE405 Artificial Intelligence
 from math import *
+from copy import *
 import random
 from portfolio import Portfolio
-from node import Node
 
 
 class SimulatedAnnealing:
@@ -12,14 +12,24 @@ class SimulatedAnnealing:
     def __init__(self, portfolio):
         self.portfolio = portfolio
 
-    def reinvest(self, next, current):
-        next.value += (current.value * 0.1)
-        current.value -= (current.value * 0.1)
+    # method invests 10% of 'current' stock into 'next' stock
+    def reinvest(self, p, next_index, curr_index):
+        p.investments[next_index].value += (p.investments[curr_index].value * 0.1)
+        p.investments[curr_index].value -= (p.investments[curr_index].value * 0.1)
 
+    # method gets the fitness of a hypothetical portfolio in which
+    # 10% of 'current' stock has been invested in the 'next' stock
+    def next_fitness(self, next_index, curr_index):
+        temp = Portfolio()
+        temp.investments = deepcopy(self.portfolio.investments)
+        self.reinvest(temp, next_index, curr_index)
+        return temp.fitness()
+
+    # method optimizes an investment portfolio using simulated annealing
     def simulated_annealing(self):
 
         # schedule maps t to temperature
-        temperature_decrease = -0.00001
+        temperature_decrease = -0.0001
         schedule = lambda t: 1 + (t * temperature_decrease)
 
         # choose random company to start with
@@ -42,17 +52,15 @@ class SimulatedAnnealing:
             # next = random node from portfolio
             next_index = random.randrange(0, 10)
             next = self.portfolio.investments[next_index]
-            # print(f'Current: {current.company} Next: {next.company}')
 
             # Calculate the value of Delta E
-            delta_E = next.percent_change - current.percent_change
-            # print(f'Delta E is {delta_E}')
+            curr_fitness = self.portfolio.fitness()
+            new_fitness = self.next_fitness(next_index, curr_index)
+            delta_E = new_fitness - curr_fitness
 
-            # If (Delta E > 0) or if [(e^(Delta E) / T) < 1], choose the next investment
-            if delta_E > 0 or (exp(delta_E / T) < 1):
-                self.reinvest(next, current)
-                # print(f'10% of {current.company} has been reinvested in {next.company}')
-                # print(f'{current.company}: {current.value:0.2f} {next.company}: {next.value:0.2f}')
+            # Reinvest if delta E > 0, or with probability of e^(delta E / T)
+            if delta_E > 0 or random.random() < exp(delta_E / T):
+                self.reinvest(self.portfolio, next_index, curr_index)
                 current = next
 
             t += 1
